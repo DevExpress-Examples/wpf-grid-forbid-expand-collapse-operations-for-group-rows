b4d2f93b79cf89b107473bd142b764723373411c
DevExpressExampleBot <fakeEmail>
2022-07-01 12:23:45 +0400

    Source auto update [skip ci]

--- VB/DXGrid_PreventGroupRowFromCollapsing/Window1.xaml.vb
+++ VB/DXGrid_PreventGroupRowFromCollapsing/Window1.xaml.vb
@@ -1,22 +1,19 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 Imports DXExample.DemoData
 
 Namespace DXGrid_PreventGroupRowFromCollapsing
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			gridControl1.ItemsSource = Invoice.GetData()
-		End Sub
-		Private Sub gridControl1_GroupRowCollapsing(ByVal sender As Object, _
-				ByVal e As RowAllowEventArgs)
-			If e.Row Is Nothing OrElse _
-				(CType(e.Row, Invoice)).Status = InvoiceStatus.Invalidated AndAlso _
-				colStatus.GroupIndex <> -1 Then
-					e.Allow = False
-			End If
-		End Sub
-	End Class
+
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.gridControl1.ItemsSource = Invoice.GetData()
+        End Sub
+
+        Private Sub gridControl1_GroupRowCollapsing(ByVal sender As Object, ByVal e As RowAllowEventArgs)
+            If e.Row Is Nothing OrElse CType(e.Row, Invoice).Status = InvoiceStatus.Invalidated AndAlso Me.colStatus.GroupIndex <> -1 Then e.Allow = False
+        End Sub
+    End Class
 End Namespace
